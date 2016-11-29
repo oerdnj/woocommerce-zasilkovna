0cd7e3b083e169cd461095e3eca9729cf0f4b61c
Ondřej Surý <ondrej@sury.org>
2016-11-29 22:24:30 +0100

    Remove cruft and add debug_mode

--- class-zasilkovna.php
+++ class-zasilkovna.php
@@ -89,6 +89,14 @@ class WC_Zasilkovna_Shipping_Method extends WC_Shipping_Method {
         );
     }
 
+    public function is_available( $package ) {
+        if ( $this->debug_mode === 'yes' ) {
+            return current_user_can('administrator');
+        }
+
+        return true;
+    }
+
     public function admin_options() {
         ?>
         <h3><?php _e( 'Zásilkovna Settings', 'woocommerce-zasilkovna-shipping-method' ); ?></h3>
@@ -109,77 +117,9 @@ class WC_Zasilkovna_Shipping_Method extends WC_Shipping_Method {
                 <?php echo $this->get_admin_options_html(); ?>
               </table><!--/.form-table-->
             </div>
-            <div id="postbox-container-1" class="postbox-container">
-              <div id="side-sortables" class="meta-box-sortables ui-sortable">
-                <div class="postbox ">
-                  <div class="handlediv" title="Click to toggle"><br></div>
-                  <h3 class="hndle"><span><i class="dashicons dashicons-update"></i>&nbsp;&nbsp;Upgrade to Pro</span></h3>
-                  <div class="inside">
-                    <div class="support-widget">
-                      <ul>
-                        <li>» International Shipping</li>
-                        <li>» Extra Domestic Options</li>
-                        <li>» Prepaid Bags Support</li>
-                        <li>» Dropshipping Support</li>
-                        <li>» Handling Fees Support</li>
-                        <li>» Auto Hassle-Free Updates</li>
-                        <li>» High Priority Customer Support</li>
-                      </ul>
-                      <a href="https://wpruby.com/plugin/woocommerce-new-zealand-post-shipping-method-pro/" class="button wpruby_button" target="_blank"><span class="dashicons dashicons-star-filled"></span> Upgrade Now</a>
-                    </div>
-                  </div>
-                </div>
-                <div class="postbox ">
-                  <div class="handlediv" title="Click to toggle"><br></div>
-                  <h3 class="hndle"><span><i class="dashicons dashicons-editor-help"></i>&nbsp;&nbsp;Plugin Support</span></h3>
-                  <div class="inside">
-                    <div class="support-widget">
-                      <p>
-                        <img style="width: 70%;margin: 0 auto;position: relative;display: inherit;" src="https://wpruby.com/wp-content/uploads/2016/03/wpruby_logo_with_ruby_color-300x88.png">
-                        <br/>
-                        Got a Question, Idea, Problem or Praise?</p>
-                      <ul>
-                        <li>» <a target="_blank" href="https://www.nzpost.co.nz/tools/rate-finder/sending-nz">Weight and Size Guidlines </a>on Zásilkovna Post website.</li>
-                        <li>» Please leave us a <a target="_blank" href="https://wordpress.org/support/view/plugin-reviews/zasilkovna-woocommerce-shipping-method?filter=5">★★★★★</a> rating.</li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-                <div class="postbox rss-postbox">
-                  <div class="handlediv" title="Click to toggle"><br></div>
-                  <h3 class="hndle"><span><i class="fa fa-wordpress"></i>&nbsp;&nbsp;WPRuby Blog</span></h3>
-                  <div class="inside">
-                    <div class="rss-widget">
-                    <?php
-                      wp_widget_rss_output(array(
-                        'url' => 'https://wpruby.com/feed/',
-                        'title' => 'WPRuby Blog',
-                        'items' => 3,
-                        'show_summary' => 0,
-                        'show_author' => 0,
-                        'show_date' => 1,
-                       ));
-                    ?>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
         <div class="clear"></div>
-        <style type="text/css">
-          .wpruby_button{
-             background-color:#4CAF50 !important;
-             border-color:#4CAF50 !important;
-             color:#ffffff !important;
-             width:100%;
-             padding:5px !important;
-             text-align:center;
-             height:35px !important;
-             font-size:12pt !important;
-           }
-        </style>
         <?php
     }
     
